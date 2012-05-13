4881ddaefc6bddb854e53cf70681a5549b0b1bf9
Michael Paterson <michael.l.paterson@gmail.com>
2012-05-13 18:41:44 -0400

    Readding correct repo files

---
+++ SisoDb/Episode 2 - Ioc/CoreConcepts/CoreConcepts/Controllers/CustomerController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CoreConcepts.Models;
+using SisoDb;
+
+namespace CoreConcepts.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly ISession _dbSession;
+
+        public CustomerController(ISession dbSession)
+        {
+            _dbSession = dbSession;
+        }
+
+        public ActionResult Add()
+        {
+            var customer = new Customer();
+
+            return View("Add", customer);
+        }
+
+        [HttpPost]
+        public ActionResult Add(Customer customer)
+        {
+            _dbSession.Insert<Customer>(customer);
+            return RedirectToAction("List");
+        }
+
+        public ActionResult List()
+        {
+            var customers = _dbSession
+                .Query<Customer>()
+                .ToArrayOf(c => new Customer
+                {
+                    Id = c.Id,
+                    Name = c.Name
+                });
+
+            return View(customers);
+        }
+    }
+}
