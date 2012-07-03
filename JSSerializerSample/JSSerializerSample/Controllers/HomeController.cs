37426dd6cbf2b945c2eef0f0bdf841485d545738
Michael Paterson <michael.l.paterson@gmail.com>
2012-07-03 17:14:59 -0400

    Added JSSerializerSample

---
+++ JSSerializerSample/JSSerializerSample/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JSSerializerSample.Models;
+
+namespace JSSerializerSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            var model = new IndexViewModel 
+            { 
+                Message = "Here is a message!",
+                UserName = "mpaterson"
+            };
+
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ JSSerializerSample/JSSerializerSample/Models/IndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JSSerializerSample.Models
+{
+    public class IndexViewModel : BaseViewModel
+    {
+        public string Message { get; set; }
+        public string UserName { get; set; }
+
+
+    }
+}
